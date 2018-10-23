e49bac29335e30462eab0408bb0505709bd2cd1e
H. Turgut Uyar <uyar@tekir.org>
2018-10-23 21:31:28 +0300

    Atölye kartlarını modal hale getir

---
+++ assets/static/modal.js
@@ -0,0 +1,20 @@
+function activate() {
+  document.querySelector('#modal-'+ this.id).classList.toggle('is-active');
+}
+
+function deactivate() {
+  ms = document.querySelectorAll('.modal.is-active')
+  for (i = 0; i < ms.length; i++) {
+    ms[i].classList.toggle('is-active')
+  }
+}
+
+ws = document.querySelectorAll('.card.workshop')
+for (i = 0; i < ws.length; i++) {
+  ws[i].addEventListener('click', activate)
+}
+
+xs = document.querySelectorAll('.modal-close')
+for (i = 0; i < xs.length; i++) {
+  xs[i].addEventListener('click', deactivate)
+}
