b05f6f81ab1b5c705e55e65f3ba50ee5991b8db0
H. Turgut Uyar <uyar@tekir.org>
2019-07-30 16:00:35 +0300

    Cosmo yerine özel Bulma CSS oluştur

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+
+gulp.task('css', function () {
+    const sass = require('gulp-sass');
+    sass.compiler = require('node-sass');
+
+    const postcss = require('gulp-postcss');
+    const uncss = require('postcss-uncss');
+
+    return gulp.src('./theme/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(postcss([uncss({html: ['./_build/**/*.html']})]))
+        .pipe(gulp.dest('./theme/css/'));
+});
+
+gulp.task('cssmin', function () {
+    const cssmin = require('gulp-cssmin');
+
+    return gulp.src('./theme/css/*.css')
+        .pipe(cssmin())
+        .pipe(gulp.dest('./assets/static/css/'));
+});
+
+gulp.task('build', gulp.series('css', 'cssmin'));
+gulp.task('watch', function () {
+    return gulp.watch('./theme/sass/**/*.scss', gulp.series('css', 'cssmin'));
+});
+
+gulp.task('default', gulp.series('build'));
