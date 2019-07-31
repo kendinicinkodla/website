ba378dfcc42a4aa5c8c921bfea2f6fb8ce8575f3
H. Turgut Uyar <uyar@tekir.org>
2019-07-31 15:05:35 +0300

    Ssass lektor'dan önce, uncss ve cssmin sonra çalışsın

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,30 @@
 var gulp = require('gulp');
 
-gulp.task('css', function () {
+gulp.task('sass', function () {
     const sass = require('gulp-sass');
     sass.compiler = require('node-sass');
-
-    const postcss = require('gulp-postcss');
-    const uncss = require('postcss-uncss');
-
     return gulp.src('./theme/sass/**/*.scss')
         .pipe(sass())
+        .pipe(gulp.dest('./assets/static/css/'));
+});
+
+gulp.task('uncss', function () {
+    const postcss = require('gulp-postcss');
+    const uncss = require('postcss-uncss');
+    return gulp.src('./assets/static/css/kendinicinkodla.css')
         .pipe(postcss([uncss({html: ['./_build/**/*.html']})]))
         .pipe(gulp.dest('./theme/css/'));
 });
 
 gulp.task('cssmin', function () {
     const cssmin = require('gulp-cssmin');
-
-    return gulp.src('./theme/css/*.css')
+    return gulp.src('./theme/css/kendinicinkodla.css')
         .pipe(cssmin())
         .pipe(gulp.dest('./assets/static/css/'));
 });
 
-gulp.task('build', gulp.series('css', 'cssmin'));
+gulp.task('build', gulp.series('sass'));
+gulp.task('build_after', gulp.series('uncss', 'cssmin'));
 gulp.task('watch', function () {
     return gulp.watch('./theme/sass/**/*.scss', gulp.series('css', 'cssmin'));
 });
